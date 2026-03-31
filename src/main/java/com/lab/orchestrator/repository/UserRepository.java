13816cc5ccde23c082709e0a6f3fd92a57821c9c
arking115 <rusalexandru115@gmail.com>
2026-03-31 11:51:52 +0300

    added dependencies and model for auth

---
+++ src/main/java/com/lab/orchestrator/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lab.orchestrator.repository;
+
+import com.lab.orchestrator.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
+
